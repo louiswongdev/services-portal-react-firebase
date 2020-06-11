dd572c9a43db9c2ef679bfa492604e672d107388
Louis <louis@Louiss-Mac-mini.local>
2020-06-11 16:31:30 -0400

    Added html to Login and Register components. Installed react-hook-form for form validation

---
+++ src/components/auth/RegisterForm.js
@@ -0,0 +1,145 @@
+/* eslint-disable no-useless-escape */
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const RegisterForm = () => {
+  const { register, errors, handleSubmit } = useForm();
+
+  const getFormData = data => {
+    console.log(data);
+  };
+
+  const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const avatarPattern = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
+
+  return (
+    <form onSubmit={handleSubmit(getFormData)}>
+      <div className="field">
+        <div className="control">
+          <input
+            ref={register({ required: true, pattern: emailPattern })}
+            name="email"
+            className="input is-large"
+            type="email"
+            placeholder="Your Email"
+            autoComplete="email"
+          />
+          {errors.email && (
+            <div className="form-error">
+              {errors.email.type === 'required' && (
+                <span className="help is-danger">Email is required</span>
+              )}
+              {errors.email.type === 'pattern' && (
+                <span className="help is-danger">
+                  Email address is not valid
+                </span>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="field">
+        <div className="control">
+          <input
+            ref={register({ required: true, minLength: 8 })}
+            name="fullName"
+            className="input is-large"
+            type="text"
+            placeholder="Full Name"
+          />
+          {errors.fullName && (
+            <div className="form-error">
+              {errors.fullName.type === 'required' && (
+                <span className="help is-danger">Name is required</span>
+              )}
+              {errors.fullName.type === 'minLength' && (
+                <span className="help is-danger">
+                  Name needs to be at least 8 characters
+                </span>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="field">
+        <div className="control">
+          <input
+            ref={register({ required: true, pattern: avatarPattern })}
+            name="avatar"
+            className="input is-large"
+            type="text"
+            placeholder="Avatar"
+          />
+          {errors.avatar && (
+            <div className="form-error">
+              {errors.avatar.type === 'required' && (
+                <span className="help is-danger">Avatar is required</span>
+              )}
+              {errors.avatar.type === 'pattern' && (
+                <span className="help is-danger">Avatart is not valid</span>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="field">
+        <div className="control">
+          <input
+            ref={register({ required: true, minLength: 6 })}
+            name="password"
+            className="input is-large"
+            type="password"
+            placeholder="Your Password"
+            autoComplete="current-password"
+          />
+          {errors.password && (
+            <div className="form-error">
+              {errors.password.type === 'required' && (
+                <span className="help is-danger">Password is required</span>
+              )}
+              {errors.password.type === 'minLength' && (
+                <span className="help is-danger">
+                  Password needs to be at least 6 characters
+                </span>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="field">
+        <div className="control">
+          <input
+            ref={register({ required: true, minLength: 6 })}
+            name="passwordConfirmation"
+            className="input is-large"
+            type="password"
+            placeholder="Repeat Password"
+            autoComplete="current-password"
+          />
+          {errors.passwordConfirmation && (
+            <div className="form-error">
+              {errors.passwordConfirmation.type === 'required' && (
+                <span className="help is-danger">
+                  Password Confirmation is required
+                </span>
+              )}
+              {errors.passwordConfirmation.type === 'minLength' && (
+                <span className="help is-danger">
+                  Password needs to be at least 6 characters
+                </span>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      <button
+        type="submit"
+        className="button is-block is-info is-large is-fullwidth"
+      >
+        Register
+      </button>
+    </form>
+  );
+};
+
+export default RegisterForm;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,27 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import RegisterForm from '../components/auth/RegisterForm';
 
 const Register = () => {
   return (
-    <div>
-      <h1>REGISTERRRR</h1>
+    <div className="auth-page">
+      <div className="container has-text-centered">
+        <div className="column is-4 is-offset-4">
+          <h3 className="title has-text-grey">Register</h3>
+          <p className="subtitle has-text-grey">Please Register to proceed.</p>
+          <div className="box">
+            <figure className="avatar">
+              <img src="https://placehold.it/128x128" alt="logo" />
+            </figure>
+            <RegisterForm />
+          </div>
+          <p className="has-text-grey">
+            <a>Sign In With Google</a>&nbsp;
+            <a href="/">Sign Up</a> &nbsp;·&nbsp;
+            <a href="../">Need Help?</a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
