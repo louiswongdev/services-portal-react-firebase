f46ca3328aae4858647ee0f579000d4fc9d1d9b5
Louis <louis@Louiss-Mac-mini.local>
2020-06-11 15:23:50 -0400

    Added redux-logger and replaced custom logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import serviceApp from '../reducers';
-
-const addLoggerToDispatch = store => {
-  const dispatch = store.dispatch;
-
-  return action => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: MEDIUMPURPLE', store.getState());
-    console.log('%c action', 'color: DEEPSKYBLUE', action);
-    const returnValue = dispatch(action);
-    console.log('%c next state', 'color: DARKORANGE', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-};
 
 const middlewares = [thunk];
 
-// const rootReducer = combineReducers({
-//   service: servicesReducer,
-// });
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(logger);
+}
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -28,9 +15,5 @@ const store = createStore(
   serviceApp,
   composeEnhancers(applyMiddleware(...middlewares)),
 );
-
-if (process.env.NODE_ENV !== 'production') {
-  store.dispatch = addLoggerToDispatch(store);
-}
 
 export default store;
