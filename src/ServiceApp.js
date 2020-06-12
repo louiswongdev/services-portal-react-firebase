4675ae54828dd56ccd05d87739f67e56a67485a5
Louis <louis@Louiss-Mac-mini.local>
2020-06-12 15:02:18 -0400

    Save logged in user profile to redux state (auth)

--- src/ServiceApp.js
+++ src/ServiceApp.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 import Routes from './Routes';
+import { useDispatch } from 'react-redux';
+import { onAuthStateChanged, storeAuthUser } from './actions';
 
 const ServiceApp = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribeAuth = onAuthStateChanged(authUser => {
+      dispatch(storeAuthUser(authUser));
+    });
+
+    return () => unsubscribeAuth();
+  }, [dispatch]);
   const RenderApplication = () => (
     <>
       <Navbar />
--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-
+import { SET_AUTH_USER } from '../types';
 import db from '../db';
 
 /**
@@ -38,7 +38,24 @@ export const register = async ({ email, password, fullName, avatar }) => {
  */
 export const createUserProfile = async userProfile => {
   try {
-    db.collection('profile').doc(userProfile.uid).set(userProfile);
+    db.collection('profiles').doc(userProfile.uid).set(userProfile);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/**
+ * ------------------------------------------
+ * Get User Profile
+ * ------------------------------------------
+ */
+export const getUserProfile = async uid => {
+  try {
+    const snapshot = await db.collection('profiles').doc(uid).get();
+
+    const userProfile = { uid, ...snapshot.data() };
+
+    return userProfile;
   } catch (error) {
     console.log(error);
   }
@@ -55,4 +72,27 @@ export const login = async ({ email, password }) => {
   } catch (error) {
     throw new Error(error.message);
   }
+};
+
+/**
+ * ------------------------------------------
+ * Auth state change
+ * ------------------------------------------
+ */
+export const onAuthStateChanged = authCB =>
+  firebase.auth().onAuthStateChanged(authCB);
+
+export const storeAuthUser = authUser => {
+  return async dispatch => {
+    if (authUser) {
+      try {
+        const userWithProfile = await getUserProfile(authUser.uid);
+        dispatch({ type: SET_AUTH_USER, user: userWithProfile });
+      } catch (error) {
+        console.log(error.message);
+      }
+    } else {
+      dispatch({ type: SET_AUTH_USER, user: null });
+    }
+  };
 };
